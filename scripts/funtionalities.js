a1f2a2e830a125e330eb66202ed51d2205fa981c
MursalinDevOps <mursalin1768@gmail.com>
2024-09-23 12:25:18 +0600

    calculation functionality added for all sections

--- scripts/funtionalities.js
+++ scripts/funtionalities.js
@@ -1,41 +1,107 @@
-
-
+// Noakhali
 document.getElementById("donate-btn1").addEventListener("click", function (e) {
-  e.preventDefault();
-  const donateAmount1 = getInputValue("donate-field1");
-  const donateAmount2 = getInputValue("donate-field2");
-  const initialDonation = getInnerTextById("noakhali-donation");
-  const availableBalance = parseInt(
-    document.getElementById("available-balance").innerText
-  );
+    e.preventDefault();
+    const donateAmount = getInputValue("donate-field1");
+    const initialDonation = getInnerTextById("noakhali-donation");
+    const availableBalance = parseInt(
+        document.getElementById("available-balance").innerText
+    );
 
-  if (donateAmount1 > availableBalance) {
-    alert("Insufficient Balance, Add money and try again.");
-    return;
-  };
-  if (isNaN(donateAmount1) || donateAmount1 < 0) {
-    alert("Invalid donate amount. Try again.");
-  } else {
-    // Update the Donation Amount
-    document.getElementById("noakhali-donation").innerText =
-      initialDonation + donateAmount1;
-    //   Update the Available Amount
-    document.getElementById("available-balance").innerText =
-      availableBalance - donateAmount1;
-  };
+    if (donateAmount > availableBalance) {
+        alert("Insufficient Balance, Add money and try again.");
+        return;
+    }
+    if (isNaN(donateAmount) || donateAmount < 0) {
+        alert("Invalid donate amount. Try again.");
+    } else {
+        // Update the Donation Amount
+        document.getElementById("noakhali-donation").innerText =
+            initialDonation + donateAmount;
+        //   Update the Available Amount
+        document.getElementById("available-balance").innerText =
+            availableBalance - donateAmount;
+    }
+});
+//   Feni
 
+document.getElementById("donate-btn2").addEventListener("click", function (e) {
+    e.preventDefault();
+    const donateAmount = getInputValue("donate-field2");
+    const initialDonation = getInnerTextById("feni-donation");
+    const availableBalance = parseInt(
+        document.getElementById("available-balance").innerText
+    );
 
+    if (donateAmount > availableBalance) {
+        alert("Insufficient Balance, Add money and try again.");
+        return;
+    }
+    if (isNaN(donateAmount) || donateAmount < 0) {
+        alert("Invalid donate amount. Try again.");
+    } else {
+        // Update the Donation Amount
+        document.getElementById("feni-donation").innerText =
+            initialDonation + donateAmount;
+        //   Update the Available Amount
+        document.getElementById("available-balance").innerText =
+            availableBalance - donateAmount;
+    }
+});
+// Quota Movement
+document.getElementById('donate-btn3').addEventListener('click', function (e) {
+    e.preventDefault();
+    const donateAmount = getInputValue('donate-field3');
+    const initialDonation = getInnerTextById('quota-donation');
+    const availableBalance = parseInt(
+        document.getElementById("available-balance").innerText
+    ); 
 
+    if (donateAmount > availableBalance) {
+        alert("Insufficient Balance, Add money and try again.");
+        return;
+    }
+    if (isNaN(donateAmount) || donateAmount < 0) {
+        alert("Invalid donate amount. Try again.");
+    } else {
+        // Update the Donation Amount
+        document.getElementById("quota-donation").innerText =
+            initialDonation + donateAmount;
+        //   Update the Available Amount
+        document.getElementById("available-balance").innerText =
+            availableBalance - donateAmount;
+    }
 
+});
 
+//   Toggle
 
+// document.getElementById("btn-donation").addEventListener("click", function () {
+//     btnToggle("card-section");
+//   });
+//   document.getElementById("btn-history").addEventListener("click", function () {
+//     btnToggle("history-section");
+//   });
+
+// document.getElementById('btn-history').addEventListener('click', function(){
+//     document.getElementById('history-section').classList.remove('hidden');
+//     document.getElementById('card-section').classList.add('hidden');
+
+// });
+
+// document.getElementById('btn-donation').addEventListener('click', function(){
+//     document.getElementById('card-section').classList.add('hidden');
+//     document.getElementById('history-section').classList.add('hidden');
+// })
 
 //   History Section
-  const place = document.getElementById('noakhali').innerText;
-  
-  document.getElementById('history-contents').innerHTML = `
- <h3>${donateAmount} Taka is donated for ${place}</h3>
- <br>
- <h3>Time</h3>
-`
-});
+
+const place = document.getElementById("noakhali").innerText;
+const currentDateTime = getCurrentDateTime();
+
+document.getElementById("history-section").innerHTML += `
+
+<div id="history-contents" class="border border-2 flex flex-col justify-center lg:w-[1152px] lg:h-[136px] p-4 my-8 rounded-xl mx-auto">
+          <h3>${donateAmount} Taka is donated for ${place}</h3>
+ <h3>Time: ${currentDateTime}</h3>
+        </div>
+`;
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -7,3 +7,16 @@ function getInnerTextById(id){
     initialDonation =  parseInt(document.getElementById(id).innerText);
     return initialDonation;
 }
+
+// Button Toggle
+function btnToggle(id){
+    document.getElementById('card-section').classList.add('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+    // Toggle
+    document.getElementById(id).classList.remove('hidden');
+}
+// Current Time
+function getCurrentDateTime() {
+    const now = new Date();
+    return now.toString(); // This will return the full date format
+}
